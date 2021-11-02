6a38eaf3d26dd96be039864f2bbf7a8f51640ca8
Tuan <trituan39@gmail.com>
2021-11-02 18:27:16 +0700

    add waypoint and enemy

---
+++ Assets/Script/Enemy.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 0.5f;
+
+    private Transform target;
+    private int wavePointIndex = 0;
+
+	void Start()
+	{
+		target = Waypoint.points[0];
+	}
+	void Update()
+	{
+		Vector3 dir = target.position - transform.position;
+		transform.Translate(dir.normalized * speed * Time.deltaTime,Space.World);
+
+		if(Vector2.Distance(transform.position, target.position) <= 0.2f)
+		{
+			GetNextWavePoint();
+		}
+	}
+	void GetNextWavePoint()
+	{ 
+		if(wavePointIndex >= Waypoint.points.Length - 1)
+		{
+			Destroy(gameObject);
+			return;
+		}
+		wavePointIndex++;
+		target = Waypoint.points[wavePointIndex];
+	}
+}
--- Assets/Script/Pathway/Waypoint.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-// Tao waypoint de track toi
-public class Waypoint : MonoBehaviour
-{
-
-}
