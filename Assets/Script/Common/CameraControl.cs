3f79da27d2eb46825c221cdbf7ded523fefed9bd
Tuan <trituan39@gmail.com>
2021-11-04 19:05:11 +0700

    add MainMenu & CameraControl

---
+++ Assets/Script/Common/CameraControl.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Camera control script.
+/// </summary>
+public class CameraControl : MonoBehaviour
+{
+    // Start view width
+    private int defaultWidth;
+    // Width on last change
+    private int lastUpdatedWidth;
+    // Start camera size
+    private float defaultCameraSize;
+
+    /// <summary>
+    /// Start this instance.
+    /// </summary>
+    void Start()
+    {
+        defaultWidth = lastUpdatedWidth = Camera.main.pixelWidth;
+        defaultCameraSize = Camera.main.orthographicSize;
+        ChangeCameraSize();
+    }
+
+    /// <summary>
+    /// Update this instance.
+    /// </summary>
+    void Update()
+    {
+        if (lastUpdatedWidth != Camera.main.pixelWidth)
+        {
+            lastUpdatedWidth = Camera.main.pixelWidth;
+            ChangeCameraSize();
+        }
+    }
+
+    /// <summary>
+    /// Change camera size to keep same view width.
+    /// </summary>
+    private void ChangeCameraSize()
+    {
+        Camera.main.orthographicSize = defaultCameraSize * ((float)defaultWidth / (float)lastUpdatedWidth);
+    }
+}
---
+++ Assets/Script/Common/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Main menu operate.
+/// </summary>
+public class MainMenu : MonoBehaviour
+{
+    /// <summary>
+    /// Load level.
+    /// </summary>
+    public void NewGame()
+    {
+        SceneManager.LoadScene("Level1", LoadSceneMode.Single);
+    }
+
+    /// <summary>
+    /// Close application.
+    /// </summary>
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
