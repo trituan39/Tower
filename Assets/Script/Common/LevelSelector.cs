49b50a11ebe0227f825ed9691e5f16305c38033b
Tuan <trituan39@gmail.com>
2021-11-15 18:22:10 +0700

    add Level

--- Assets/Script/Common/LevelSelector.cs
+++ Assets/Script/Common/LevelSelector.cs
@@ -10,13 +10,17 @@ public class LevelSelector : MonoBehaviour
 	{
 		int levelReached = PlayerPrefs.GetInt("levelReached", 1);
 
-		for(int i = 0; i< levelButton.Length;i++)
+		for(int i = 0; i < levelButton.Length;i++)
 		{
 			if(i + 1 > levelReached)
 			levelButton[i].interactable = false;
 		}
 	}
 	public void Select(string levelName)
+	{
+		SceneManager.LoadScene(levelName);
+	}
+	public void Back(string levelName)
 	{
 		SceneManager.LoadScene(levelName);
 	}
